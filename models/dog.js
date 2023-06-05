f2937958533b6df619182b2acc3a888a88377c8d
nicholasL <nicklohmann@msn.com>
2023-06-05 12:39:07 -0500

    add dog model

---
+++ models/dog.js
@@ -0,0 +1,48 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Dog extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Dog.init({
+    breed: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      onDelete: 'CASCADE'
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      onDelete: 'CASCADE'
+    },
+    photo: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      onDelete: 'CASCADE'
+    },
+  		// profileId: { type: Sequelize.INTEGER },
+      profileId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        onDelete: 'CASCADE',
+        references: {
+          model: 'Profiles',
+          key: 'id',
+        },
+      },
+    
+  }, {
+    sequelize,
+    modelName: 'Dog',
+  });
+  return Dog;
+};
