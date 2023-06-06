559a28e4151dac4e5286a31997d84b6b20d905fb
nicholasL <nicklohmann@msn.com>
2023-06-06 07:51:02 -0500

    add votes to database

---
+++ migrations/20230604165932-create-vote.js
@@ -0,0 +1,48 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Votes', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      value: {
+        type: Sequelize.INTEGER,
+        validate: {
+          min: 0,
+          max: 5,
+        },
+      },
+      dogId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'Dogs',
+          key: 'id',
+        },
+      },
+      voterId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'Profiles',
+          key: 'id',
+        },
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Votes');
+  }
+};
